c82d495ffa80dc7be1b6bfd934ea7914f1f1386c
tokaretin <tokaretin@gmail.com>
2023-09-15 04:20:30 +0200

    Добавьте файлы проекта.

---
+++ 18-1-Default-argument-values/18-1-Default-argument-values.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+#include <string>
+
+void foo(int i = 4, float j = 4.3, std::string str = "Hello World")
+{
+    std::cout << i << " " << j << " " << str << std::endl;
+}
+
+int main()
+{
+    foo();
+}
+
